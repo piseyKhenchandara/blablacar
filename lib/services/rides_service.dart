3436f82afd469a7d9b7001e35343835307d96c02
Khen chandarapisey <piseykhenchandara93@gmail.com>
2026-02-05 11:19:41 +0700

    Bla-001

---
+++ lib/services/rides_service.dart
@@ -0,0 +1,42 @@
+import '../dummy_data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+
+  static List<Ride> filterByDeparture(Location departure) {
+    return availableRides
+        .where((ride) => ride.departureLocation == departure)
+        .toList();
+  }
+
+  static List<Ride> filterBySeatRequested(Location departure) {
+    return availableRides
+        .where((ride) => ride.remainingSeats == departure)
+        .toList();
+  }
+
+   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return availableRides.where((ride) {
+      if (departure != null && ride.departureLocation != departure) {
+        return false;
+      }
+
+      if (seatRequested != null && ride.remainingSeats < seatRequested) {
+        return false;
+      }
+
+      return true;
+    }).toList();
+  }
+
+}
+
+
+
+ 
